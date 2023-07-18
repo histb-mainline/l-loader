0852da3c782a9ec92b42e32cb33c5362988d99b1
185264646 <43387518+185264646@users.noreply.github.com>
2023-07-17 19:46:15 -0500

    Update utils.h
    fix prev commit

--- utils.h
+++ utils.h
@@ -1,5 +1,5 @@
-#define writel(val, addr) *(volatile unsigned int *)(y) = (x)
-#define readl(addr) *(volatile unsigned int *)(x)
+#define writel(val, addr) *(volatile unsigned int *)(addr) = (val)
+#define readl(addr) *(volatile unsigned int *)(val)
 #define mdelay(s) udelay((s) * 1000)
 void udelay(unsigned int usec);
 void _Noreturn panic(void);
