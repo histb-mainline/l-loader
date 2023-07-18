1eb08aaac42eb330dd037e420c20204990fc79b5
185264646 <43387518+185264646@users.noreply.github.com>
2023-07-17 19:45:46 -0500

    Update utils.h
    rename macro parameters better

--- utils.h
+++ utils.h
@@ -1,5 +1,5 @@
-#define writel(x, y) *(volatile unsigned int *)(y) = (x)
-#define readl(x) *(volatile unsigned int *)(x)
+#define writel(val, addr) *(volatile unsigned int *)(y) = (x)
+#define readl(addr) *(volatile unsigned int *)(x)
 #define mdelay(s) udelay((s) * 1000)
 void udelay(unsigned int usec);
 void _Noreturn panic(void);
